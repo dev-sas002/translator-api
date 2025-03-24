a8ec13c95b77824faad69b71ab057c64c37c4a63
dev-sas002 <dev-sas002@users.noreply.github.com>
2025-03-24 09:45:00 -0400

    Feature::Refactor::Made ModifiedSourceTextSerializer side-effect free

--- app/serializers/modified_source_text_serializer.rb
+++ app/serializers/modified_source_text_serializer.rb
@@ -2,13 +2,21 @@ class ModifiedSourceTextSerializer < ActiveModel::Serializer
   attributes :id, :source_language_code, :target_language_code, :source_text, :glossary_id
 
   def source_text
-    if object.glossary.present?
-      glossary_terms = object.glossary.terms.pluck(:source_term)
-      glossary_terms.each do |term|
-        object.source_text.gsub!(/(?<!\w)#{Regexp.escape(term)}(?!\w)/, "<HIGHLIGHT>#{term}</HIGHLIGHT>")
-      end
+    return object.source_text unless object.glossary.present?
+
+    # Avoid mutating `object.source_text` in-place. Serializers should be
+    # side-effect free so repeated rendering (or other consumers) doesn't
+    # depend on render order.
+    highlighted = object.source_text.dup
+    glossary_terms = object.glossary.terms.pluck(:source_term)
+
+    glossary_terms.each do |term|
+      highlighted = highlighted.gsub(
+        /(?<!\w)#{Regexp.escape(term)}(?!\w)/,
+        "<HIGHLIGHT>#{term}</HIGHLIGHT>"
+      )
     end
 
-    object.source_text
+    highlighted
   end
 end
